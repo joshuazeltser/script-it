1aafce74fccbd67560a8149cee29f53729e223ce
Joshua Zeltser <joshua.zeltser@gmail.com>
2021-08-24 21:21:01 +0300

    added basic error handling

--- script-it.sh
+++ script-it.sh
@@ -7,5 +7,19 @@ echo ${COMMANDS}
 HISTFILE=~/.bash_history
 set -o history
 
+if [ "$1" = "--help" ]; then
+    echo "script-it help"  
+    exit 1
+elif [ $# -lt 2 ]; then
+    echo "Invalid number of arguments. Run ./script-it --help for more info"
+    exit 1
+fi
+
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+    echo "The number of commands you added is invalid. Please run ./script-it --help for more info"
+    exit 1
+fi
+
 history | tail -n ${COMMANDS} | cut -c 8- > ${FILE_NAME}
 chmod +x ${FILE_NAME}
